3223e2058d0b5b379c567cafa5d5233e8cb554ff
Genk <lpj008@126.com>
2019-04-26 16:02:46 +0800

    Update Processing_Im.m

--- Training_Code/Processing_Im.m
+++ Training_Code/Processing_Im.m
@@ -5,12 +5,12 @@ function output1 = Processing_Im(im, net, gpu, out_idx)
     
     input = im2single(im(:,:,1));
     if gpu
-        %input = gpuArray(input*4-2);
-		input = gpuArray(input);
+        input = gpuArray(input*4-2);
+	%	input = gpuArray(input);
     end
     net.eval({'input',input}) ;
     %%% output (single)
-    output1 = gather(squeeze(gather(net.vars(out_idx).value)));
+    output1 = gather(squeeze(gather(net.vars(out_idx).value+2)/4));
     output1 = im2uint8(output1);
 end
 
